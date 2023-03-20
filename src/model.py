e6dd7320afe69b81f86c708040a80972569bea7d
Aleksander Piciga <aleksander.piciga@gmail.com>
2023-03-20 15:17:43 +0100

    updated model parameters

--- src/model.py
+++ src/model.py
@@ -3,10 +3,10 @@ import torch_geometric
 
 
 class GNN(torch.nn.Module):
-    def __init__(self, hidden_channels):
+    def __init__(self, hidden_channels, dropout):
         super().__init__()
-        self.conv1 = torch_geometric.nn.SAGEConv((-1, -1), hidden_channels, normalize=True)
-        self.conv2 = torch_geometric.nn.SAGEConv((-1, -1), hidden_channels, normalize=True)
+        self.conv1 = torch_geometric.nn.SAGEConv((-1, -1), hidden_channels, normalize=True, dropout=dropout)
+        self.conv2 = torch_geometric.nn.SAGEConv((-1, -1), hidden_channels, normalize=True, dropout=dropout)
 
     def forward(self, x, edge_index):
         x = self.conv1(x, edge_index).relu()
@@ -22,20 +22,18 @@ class LinkPredictor(torch.nn.Module):
         return (playlist_embedding * track_embedding).sum(dim=-1)
 
 class HeteroModel(torch.nn.Module):
-    def __init__(self, hidden_channels, node_features, metadata):
+    def __init__(self, hidden_channels, node_features, metadata, dropout=0):
         super().__init__()
         # Since the dataset does not come with rich features, we also learn two
         # embedding matrices for users and movies:
 
         self.node_lin = {
-            k: torch.nn.Linear(v.shape[1], hidden_channels) for k, v in node_features.items()
+            k: torch.nn.Linear(v.shape[1], hidden_channels, weight_initializer="glorot") for k, v in node_features.items()
         }
 
-        for _, v in self.node_lin.items():
-            torch.nn.init.xavier_uniform_(v.weight)
         
         # Instantiate homogeneous GNN:
-        self.gnn = GNN(hidden_channels)
+        self.gnn = GNN(hidden_channels, dropout=dropout)
         # Convert GNN model into a heterogeneous variant:
         self.gnn = torch_geometric.nn.to_hetero(self.gnn, metadata=metadata)
 
