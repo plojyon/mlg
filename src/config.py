168c300cef1792d76b858621408d617e2fbed2e1
Yon Ploj <admin@yon.si>
2023-03-20 14:00:46 +0100

    dorito

--- src/config.py
+++ src/config.py
@@ -4,11 +4,12 @@ base = "spotify_million_playlist_dataset"
 pickles = base + "/pickles"
 
 # full dataset
-dataset_path = base + "/data"
-pickled_graph = pickles + "/G.pkl"
-pickled_datasets = pickles + "/datasets.pkl"
-pickled_ghetero = pickles + "/ghetero.pkl"
-pickled_top_G = lambda i: pickles + f"/top-G-{i}.pkl"
+class big:
+  dataset_path = base + "/data"
+  pickled_graph = pickles + "/G.pkl"
+  pickled_datasets = pickles + "/datasets.pkl"
+  pickled_ghetero = pickles + "/ghetero.pkl"
+  pickled_top_G = lambda i: pickles + f"/top-G-{i}.pkl"
 
 # example dataset (override above)
 dataset_path = base + "/example"
