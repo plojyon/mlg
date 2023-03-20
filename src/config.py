435f11bbaac45d064fdc3f51cab61d5521faf2d0
Yon Ploj <admin@yon.si>
2023-03-20 13:05:13 +0100

    Moved files

---
+++ src/config.py
@@ -0,0 +1,18 @@
+"""Project configuration."""
+
+base = "spotify_million_playlist_dataset"
+pickles = base + "/pickles"
+
+# full dataset
+dataset_path = base + "/data"
+pickled_graph = pickles + "/G.pkl"
+pickled_datasets = pickles + "/datasets.pkl"
+pickled_ghetero = pickles + "/ghetero.pkl"
+pickled_top_G = lambda i: pickles + f"/top-G-{i}.pkl"
+
+# example dataset (override above)
+dataset_path = base + "/example"
+pickled_graph = pickles + "/G_example.pkl"
+pickled_datasets = pickles + "/datasets_example.pkl"
+pickled_ghetero = pickles + "/ghetero_example.pkl"
+pickled_top_G = lambda i: pickles + f"/top-G-{i}_example.pkl"
